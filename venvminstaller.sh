5e9c6c8d58998510d765df09fed3d61878f8a181
Qsenja --- MeoDT <Qsenjareal@proton.me>
2025-08-05 21:36:18 +0200

    Update venvminstaller.sh

--- venvminstaller.sh
+++ venvminstaller.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 set -e
-
-USER_HOME="$HOME"
-TARGET_DIR="$USER_HOME/.venvmanager"
-ALIAS_FILE="$TARGET_DIR/.venvmanagerrc"
-REPO_URL="https://raw.githubusercontent.com/MeoDT/Linuxutils/main/venvm.py"
-SCRIPT_NAME="venvm.py"
-BASHRC="$USER_HOME/.bashrc"
-mkdir -p "$TARGET_DIR"
-curl -fsSL "$REPO_URL" -o "$TARGET_DIR/$SCRIPT_NAME"
-chmod +x "$TARGET_DIR/$SCRIPT_NAME"
-echo "alias venvm='python3 $TARGET_DIR/$SCRIPT_NAME'" > "$ALIAS_FILE"
-echo "[OK] Alias written to $ALIAS_FILE"
-if ! grep -q "source $ALIAS_FILE" "$BASHRC"; then
-    echo -e "\n# Load venvmanager aliases\nif [ -f \"$ALIAS_FILE\" ]; then\n    source \"$ALIAS_FILE\"\nfi" >> "$BASHRC"
-    echo "[OK] Added source line to $BASHRC"
-fi
-source "$ALIAS_FILE"
-#MeoDT software 2025
-
+d="$HOME/.venvmanager"
+f="$d/venvm.py"
+c="$d/venvm_config.json"
+r="$d/.venvmanagerrc"
+a="alias venvm='python3 $f'"
+m() { p=$!; s="/-\|"; i=0; while kill -0 "$p" 2>/dev/null; do printf "\rInstalling %c" "${s:i++%${#s}:1}"; sleep 0.1; done; printf "\r"; }
+mkdir -p "$d" >/dev/null 2>&1
+( curl -sSLo "$f" https://raw.githubusercontent.com/MeoDT/Linuxutils/refs/heads/main/venvm.py && chmod +x "$f" && touch "$c" && { [ ! -f "$r" ] || ! grep -q "$a" "$r"; } && echo "$a" > "$r" ) & m
+echo "✓ Installed and ready for use"
+sleep 5
+rm -- "$0"
