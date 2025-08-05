8ed1647459f89a15162a9864313d4bac8cdeb074
Qsenja --- MeoDT <Qsenjareal@proton.me>
2025-08-05 21:40:58 +0200

    Update venvminstaller.sh

--- venvminstaller.sh
+++ venvminstaller.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+[ "$(uname)" != "Linux" ] && echo "✗ Linux only" && exit 1
 set -e
-d="$HOME/.venvmanager"
-f="$d/venvm.py"
-c="$d/venvm_config.json"
-r="$d/.venvmanagerrc"
-a="alias venvm='python3 $f'"
-m() { p=$!; s="/-\|"; i=0; while kill -0 "$p" 2>/dev/null; do printf "\rInstalling %c" "${s:i++%${#s}:1}"; sleep 0.1; done; printf "\r"; }
+d="$HOME/.venvmanager"; f="$d/venvm.py"; c="$d/venvm_config.json"; r="$d/.venvmanagerrc"; a="alias venvm='python3 $f'"
+[ -d "$d" ] && rm -rf "$d"
 mkdir -p "$d" >/dev/null 2>&1
-( curl -sSLo "$f" https://raw.githubusercontent.com/MeoDT/Linuxutils/refs/heads/main/venvm.py && chmod +x "$f" && touch "$c" && { [ ! -f "$r" ] || ! grep -q "$a" "$r"; } && echo "$a" > "$r" ) & m
+m() { p=$!; s="/-\|"; i=0; while kill -0 "$p" 2>/dev/null; do printf "\rInstalling %c" "${s:i++%4:1}"; sleep 0.1; done; printf "\r"; }
+( curl -sSLo "$f" https://raw.githubusercontent.com/MeoDT/Linuxutils/refs/heads/main/venvm.py && chmod +x "$f" && touch "$c" && sed -i "/alias venvm=/d" "$r" 2>/dev/null || true && echo "$a" > "$r" ) & m
 echo "✓ Installed and ready for use"
 sleep 5
 rm -- "$0"
