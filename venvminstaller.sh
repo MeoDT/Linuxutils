2e93d2852cbc18c0d584e8f11bd5783a9595b381
Qsenja --- MeoDT <Qsenjareal@proton.me>
2025-08-06 17:11:54 +0200

    Update venvminstaller.sh

--- venvminstaller.sh
+++ venvminstaller.sh
@@ -9,5 +9,4 @@ mkdir -p "$d" >/dev/null 2>&1
 m() { p=$!; s="/-\|"; i=0; while kill -0 "$p" 2>/dev/null; do printf "\rInstalling %c" "${s:i++%4:1}"; sleep 0.1; done; printf "\r"; }
 ( curl -sSLo "$f" https://raw.githubusercontent.com/MeoDT/Linuxutils/refs/heads/main/venvm.py && chmod +x "$f" && touch "$c" && sed -i "/alias venvm=/d" "$r" 2>/dev/null || true && echo "$a" > "$r" ) & m
 echo "✓ Installed and ready for use"
-sleep 2
 rm -- "$0"
